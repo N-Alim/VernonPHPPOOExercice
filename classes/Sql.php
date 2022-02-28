141f957c1428b8b20a2bfae036bc738d38449fc4
Nouira Alim <nouiraalim@gmail.com>
2022-02-28 12:42:22 +0100

    Ajout classe Sql
    Ajout constructeur pour instancier un PDO et ajout destructeur

---
+++ classes/Sql.php
@@ -0,0 +1,29 @@
+<?php
+
+class Sql
+{
+    private string $serverName = "localhost";
+    private string $userName = "root";
+    private string $database = "poo";
+    private string $userPassword = "";
+    private object $connexion;
+
+    public function __construct()
+    {
+        try 
+        {
+            $this->connexion = new PDO("mysql:host=$this->serverName; dbname=$this->database", $this->userName, $this->userPassword);
+            $this->connexion->setAttribute(PDO::ATTR_ERRMODE, pdo::ERRMODE_EXCEPTION);
+        }
+        
+        catch (PDOException $e)
+        {
+            die("Erreur : " . $e->getMessage());
+        }
+    }
+
+    public function __destruct()
+    {
+        unset($this->connexion);
+    }
+}
