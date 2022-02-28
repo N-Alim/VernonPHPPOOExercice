0912602485202446a7aa571ce659c9ca6199a0c5
Nouira Alim <nouiraalim@gmail.com>
2022-02-28 14:55:12 +0100

    Test de User

--- index.php
+++ index.php
@@ -4,5 +4,8 @@ session_start();
 date_default_timezone_set("Europe/Paris");
 spl_autoload_register(function ($classname) 
 {
-    include "./classes" . $classname . "php";
+    include "./classes/" . $classname . ".php";
 });
+
+$user = new User();
+var_dump($user->getNomUser());
